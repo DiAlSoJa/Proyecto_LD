3f6126fe26b89c667e62e7b347d1a3c34422d76b
Diego Alfredo Soto Jaquez <diego.soto@venteks.com>
2026-03-02 23:57:34 -0600

    se agrego el ldpapi service

---
+++ src/LD.Application/Common/Interfaces/Helpers/ICryptoService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LD.Application.Common.Interfaces.Helpers
+{
+    public interface ICryptoService
+    {
+        string Encrypt(string plainText);
+        string Decrypt(string cipherText);
+    }
+}
