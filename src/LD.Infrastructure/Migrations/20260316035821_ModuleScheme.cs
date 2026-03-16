e126858d210d7e71de054b13456ffd7fa67ce544
Juan Manuel Parvol <115371574+juanmparvol@users.noreply.github.com>
2026-03-15 22:00:00 -0600

    MigracionMarzo

---
+++ src/LD.Infrastructure/Migrations/20260316035821_ModuleScheme.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LD.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModuleScheme : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "Modules",
+                newName: "Modules",
+                newSchema: "Auth");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "Modules",
+                schema: "Auth",
+                newName: "Modules");
+        }
+    }
+}
