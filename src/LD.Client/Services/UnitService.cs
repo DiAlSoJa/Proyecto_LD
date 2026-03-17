683749d1e01bf6c83a3cd2fe91488c1822b0381c
Juan Manuel Parvol <115371574+juanmparvol@users.noreply.github.com>
2026-03-16 18:16:41 -0600

    UnitService

---
+++ src/LD.Client/Services/UnitService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LD.Forms.Configuration;
+
+namespace LD.Client.Services
+{
+    public class UnitService
+    {
+        private readonly ApiService _api;
+        private readonly ApiEndpoints _apiEndpoints;
+        public UnitService(ApiService api, ApiEndpoints apiEndpoints)
+        {
+            _api = api;
+            _apiEndpoints = apiEndpoints;
+        }
+    }
+}
