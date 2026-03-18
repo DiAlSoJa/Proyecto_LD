88864e7448a53726f4f8b09fd5591acc8020c371
Juan Manuel Parvol <115371574+juanmparvol@users.noreply.github.com>
2026-03-18 09:33:36 -0600

    Cambios,menores ,para pruebas

--- src/LD.Forms/Views/Forms/FrmLogin.Designer.cs
+++ src/LD.Forms/Views/Forms/FrmLogin.Designer.cs
@@ -16,7 +16,7 @@ namespace LD.Forms.Views.Forms
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
-            {
+            {  
                 components.Dispose();
             }
             base.Dispose(disposing);
@@ -72,7 +72,7 @@ namespace LD.Forms.Views.Forms
             label2.Name = "label2";
             label2.Size = new Size(106, 19);
             label2.TabIndex = 10;
-            label2.Text = "Version: 1.0.13";
+            label2.Text = "Version: 1.0.17";
             // 
             // label1
             // 
